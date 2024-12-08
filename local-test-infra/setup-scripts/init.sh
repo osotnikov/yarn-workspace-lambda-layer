94109cdae750171a43b6346c40f6ba008b01b7a1
osotnikov <olegksotnikov@yahoo.com>
2024-12-08 00:28:11 +0000

    bim bam boom

--- local-test-infra/setup-scripts/init.sh
+++ local-test-infra/setup-scripts/init.sh
@@ -17,7 +17,7 @@ echo $AWS_DEFAULT_REGION
 
 # initialize tables
 # create Users table
-aws dynamodb create-table --table-name Users --attribute-definitions AttributeName=username,AttributeType=S --key-schema AttributeName=username,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000 --region dummy-region | cut -c -80
+aws dynamodb create-table --table-name Users --attribute-definitions AttributeName=username,AttributeType=S --key-schema AttributeName=username,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000 --region $AWS_DEFAULT_REGION | cut -c -80
 
 # aws dynamodb list-tables --endpoint-url http://localhost:8000 --region dummy-region
 # aws dynamodb scan --endpoint-url http://localhost:8000 --table-name Users --region dummy-region
@@ -27,10 +27,10 @@ aws dynamodb create-table --table-name Users --attribute-definitions AttributeNa
 docker run -d --rm --name cog --network $DOCKER_NET --publish 9229:9229 --volume $(pwd)/.cognito:/app/.cognito jagregory/cognito-local:latest
 
 sleep 6
+ # There's no way to create user pool with custom endpoint from cdk AFAIK
+export USER_POOL_ID=`aws --endpoint http://localhost:9229 cognito-idp create-user-pool --pool-name usrp | jq -r '.UserPool.Id'`
 
-aws --endpoint http://localhost:9229 cognito-idp create-user-pool --pool-name usrp | cut -c -80
-
-aws --endpoint http://localhost:9229 cognito-idp create-user-pool-client --user-pool-id usrp --client-name usrpc | cut -c -80
+export USER_POOL_CLIENT_ID=`aws --endpoint http://localhost:9229 cognito-idp create-user-pool-client --user-pool-id $USER_POOL_ID --client-name usrpc | jq -r '.UserPoolClient.ClientId'`
 
 # build lambdas
 
@@ -42,4 +42,4 @@ aws --endpoint http://localhost:9229 cognito-idp create-user-pool-client --user-
 #npm run synth
 
 # start lambdas
-sam local start-api --warm-containers EAGER --debug --docker-network $DOCKER_NET -t ./cdk.out/YarnWorkspaceLambdaLayer.template.json --parameter-overrides ParameterKey=COGNITO_CLIENT_ID,ParameterValue=iddididid
+sam local start-api --warm-containers EAGER --debug --docker-network $DOCKER_NET -t ./cdk.out/YarnWorkspaceLambdaLayer.template.json --parameter-overrides ParameterKey=COGNITO_CLIENT_ID,ParameterValue=$USER_POOL_CLIENT_ID ParameterKey=AWS_REGION,ParameterValue=$AWS_DEFAULT_REGION
--- local-test-infra/src/infrastructure-stack.ts
+++ local-test-infra/src/infrastructure-stack.ts
@@ -1,6 +1,6 @@
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import {Code, LayerVersion, Runtime} from "aws-cdk-lib/aws-lambda";
 import * as nodeLambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -8,76 +8,77 @@ import * as path from "path";
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const dependenciesLayer = new LayerVersion(this, "DependenciesLayer", {
-      code: Code.fromAsset("../lambda-layer"),
-      compatibleRuntimes: [Runtime.NODEJS_20_X]
-    });
-
-    const lambdaAppDir = path.resolve(__dirname, '../../')
-    const aLambda: nodeLambda.NodejsFunction =
-      new nodeLambda.NodejsFunction(this, 'ALambda', {
-        functionName: `aLambda`,
-        runtime: lambda.Runtime.NODEJS_20_X,
-        layers: [dependenciesLayer],
-        depsLockFilePath: path.join(lambdaAppDir, 'yarn.lock'),
-        entry: path.join(lambdaAppDir, 'lambda-a/bin/a.js'),
-        memorySize: 1024,
-        handler: 'handler',
-        environment: {
-          LOG_LEVEL: 'DEBUG',
-        },
-      });
-
-    const bLambda: nodeLambda.NodejsFunction =
-        new nodeLambda.NodejsFunction(this, 'BLambda', {
-          functionName: `bLambda`,
-          runtime: lambda.Runtime.NODEJS_20_X,
-          layers: [dependenciesLayer],
-          depsLockFilePath: path.join(lambdaAppDir, 'yarn.lock'),
-          entry: path.join(lambdaAppDir, 'lambda-b/bin/b.js'),
-          memorySize: 1024,
-          handler: 'handler',
-          environment: {
-            LOG_LEVEL: 'DEBUG',
-            COGNITO_CLIENT_ID: ''
-          },
+        const dependenciesLayer = new LayerVersion(this, "DependenciesLayer", {
+            code: Code.fromAsset("../lambdas/lambda-layer"),
+            compatibleRuntimes: [Runtime.NODEJS_20_X]
         });
 
-    // create the api to allow us to create a new product
-    const api: apigw.RestApi = new apigw.RestApi(this, 'Api', {
-      description: `a-b gw`,
-      restApiName: `apigw`,
-      deploy: true,
-      deployOptions: {
-        stageName: 'api',
-        dataTraceEnabled: true,
-        loggingLevel: apigw.MethodLoggingLevel.INFO
-      },
-    });
+        const rootDir = path.resolve(__dirname, '../../')
+        const lambdaAppDir = path.resolve(__dirname, '../../lambdas')
+        const aLambda: nodeLambda.NodejsFunction =
+            new nodeLambda.NodejsFunction(this, 'ALambda', {
+                functionName: `aLambda`,
+                runtime: lambda.Runtime.NODEJS_20_X,
+                layers: [dependenciesLayer],
+                depsLockFilePath: path.join(rootDir, 'yarn.lock'),
+                entry: path.join(lambdaAppDir, 'lambda-a/bin/a.js'),
+                memorySize: 1024,
+                handler: 'handler',
+                environment: {
+                    LOG_LEVEL: 'DEBUG',
+                },
+            });
 
-    const apiRoot: apigw.Resource = api.root.addResource('v1');
-    const aResource: apigw.Resource = apiRoot.addResource('a');
-    aResource.addMethod(
-        'POST',
-        new apigw.LambdaIntegration(aLambda, {
-          proxy: true,
-        })
-    );
-    const bResource: apigw.Resource = apiRoot.addResource('b');
-    bResource.addMethod(
-        'POST',
-        new apigw.LambdaIntegration(bLambda, {
-          proxy: true,
-        })
-    );
+        const bLambda: nodeLambda.NodejsFunction =
+            new nodeLambda.NodejsFunction(this, 'BLambda', {
+                functionName: `bLambda`,
+                runtime: lambda.Runtime.NODEJS_20_X,
+                layers: [dependenciesLayer],
+                depsLockFilePath: path.join(rootDir, 'yarn.lock'),
+                entry: path.join(lambdaAppDir, 'lambda-b/bin/b.js'),
+                memorySize: 1024,
+                handler: 'handler',
+                environment: {
+                    LOG_LEVEL: 'DEBUG',
+                    COGNITO_CLIENT_ID: ''
+                },
+            });
 
-    // dynamodb
-      new dynamodb.Table(this, 'Table', {
-          partitionKey: { name: 'username', type: dynamodb.AttributeType.STRING },
-          billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      });
-  }
+        // create the api to allow us to create a new product
+        const api: apigw.RestApi = new apigw.RestApi(this, 'Api', {
+            description: `a-b gw`,
+            restApiName: `apigw`,
+            deploy: true,
+            deployOptions: {
+                stageName: 'api',
+                dataTraceEnabled: true,
+                loggingLevel: apigw.MethodLoggingLevel.INFO
+            },
+        });
+
+        const apiRoot: apigw.Resource = api.root.addResource('v1');
+        const aResource: apigw.Resource = apiRoot.addResource('a');
+        aResource.addMethod(
+            'POST',
+            new apigw.LambdaIntegration(aLambda, {
+                proxy: true,
+            })
+        );
+        const bResource: apigw.Resource = apiRoot.addResource('b');
+        bResource.addMethod(
+            'POST',
+            new apigw.LambdaIntegration(bLambda, {
+                proxy: true,
+            })
+        );
+
+        // dynamodb
+        new dynamodb.Table(this, 'Table', {
+            partitionKey: {name: 'username', type: dynamodb.AttributeType.STRING},
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        });
+    }
 }
